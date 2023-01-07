aadcb4ecd2edff6ad65929c1c7b48cf272e27c4b
Guilherme Silva <guidss3394@gmail.com>
2023-01-07 19:15:32 -0300

    Adicionando itens na mochila

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,21 @@
 const formulario = document.querySelector('#novoItem')
+const lista = document.querySelector('#lista')
 
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault()
 
-    console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['quantidade'].value)
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
+
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add('item')
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+    
+    lista.appendChild(novoItem)
+}
