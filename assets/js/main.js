9ffe8765eed5ce6d664cf0829e1b8fc3d45a76ee
Guilherme Silva <guidss3394@gmail.com>
2023-01-08 14:17:35 -0300

    Interagindo com dados do navegador

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,41 @@
-const formulario = document.querySelector('#novoItem')
-const lista = document.querySelector('#lista')
-const itens = []
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-formulario.addEventListener('submit', (evento) => {
+itens.forEach((elemento) => {
+    criaElemento(elemento)
+})
+
+form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
-
-    nome.value = ''
-    quantidade.value = ''
-})
-
-function criaElemento(nome, quantidade) {
-    const novoItem = document.createElement('li')
-    novoItem.classList.add('item')
-
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
-    
-    lista.appendChild(novoItem)
-
     const itemAtual = {
-        'nome': nome,
-        'quantidade': quantidade
+        "nome": nome.value,
+        "quantidade": quantidade.value
     }
+
+    criaElemento(itemAtual)
 
     itens.push(itemAtual)
 
-    localStorage.setItem('item', JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
+})
+
+function criaElemento(item) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+    novoItem.appendChild(numeroItem)
+
+    novoItem.innerHTML += item.nome
+
+    lista.appendChild(novoItem)
 }
